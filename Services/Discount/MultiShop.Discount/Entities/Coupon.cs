56099776cfc3811501cca0bf756e524e7fefaa72
akiri <akirim22@posta.pau.edu.tr>
2025-06-24 14:19:23 +0300

    Add Discount module with API, DB, and Swagger support
    Introduced a new "Discount" module to the MultiShop solution:
    - Added "MultiShop.Discount" project to the solution file.
    - Configured appsettings for logging, connection strings, and hosts.
    - Created `MultiShop.Discount.csproj` with .NET 8.0 and required packages.
    - Implemented `DapperContext` for DB connections and EF Core setup.
    - Defined `Coupon` entity and created initial database migration.
    - Added API boilerplate in `Program.cs` with Swagger and middleware.
    - Configured launch settings for local development and Swagger UI.

---
+++ Services/Discount/MultiShop.Discount/Entities/Coupon.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Discount.Entities
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string Code { get; set; }
+        public int Rate { get; set; }
+        public bool Isactive { get; set; }
+        public DateTime VaildDate { get; set; }
+
+    }
+}
